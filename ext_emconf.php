04631f269837c8b882f181229b577747008aff64
mabolek <mathias@lilio.com>
2020-06-14 17:52:20 +0200

    [TASK] Added trademark to Google Drive name

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Google Drive for FAL',
-    'description' => 'Use Google Drive as File Storage in your TYPO3 installation.',
+    'title' => 'Google Drive™ for FAL',
+    'description' => 'Use Google Drive™ as File Storage in your TYPO3 installation.',
     'category' => 'backend',
     'author' => 'Mathias Bolt Lesniak',
     'author_email' => 'mathias@pixelant.net',
