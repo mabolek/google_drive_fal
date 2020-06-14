378819523811c738bca00b238c234aff35416c2e
mabolek <mathias@lilio.com>
2020-06-14 17:56:26 +0200

    [TASK] Added API client and command line interface
    Mostly copies from georgringer/google_docs_content

---
+++ Classes/Api/Client.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types = 1);
+
+namespace Mabolek\GoogleDriveFal\Api;
+
+use Google_Service_Docs;
+use Google_Service_Drive;
+use TYPO3\CMS\Core\Registry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Client
+{
+
+    /** @var Registry */
+    protected $registry;
+
+    private const REGISTRY_KEY = 'google_drive_fal';
+
+    public const SCOPES = [
+        Google_Service_Docs::DOCUMENTS_READONLY,
+        Google_Service_Drive::DRIVE_FILE,
+        Google_Service_Drive::DRIVE_METADATA,
+        Google_Service_Drive::DRIVE];
+
+    public function __construct()
+    {
+        $this->registry = GeneralUtility::makeInstance(Registry::class);
+    }
+
+    public function setAuthConfig($credentials)
+    {
+        $this->registry->set(self::REGISTRY_KEY, 'credentials', json_encode($credentials));
+    }
+
+    public function getAuthConfig()
+    {
+        $credentials = $this->registry->get(self::REGISTRY_KEY, 'credentials');
+        if ($credentials) {
+            return json_decode($credentials, true);
+        }
+
+        return [];
+    }
+
+    /**
+     * Returns an authorized API client.
+     * @return \Google_Client the authorized client object
+     */
+    public function getClient()
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Docs Content for TYPO3');
+        $client->setScopes(self::SCOPES);
+        $client->setAccessType('offline');
+        $client->setAuthConfig($this->getAuthConfig());
+
+        $accessToken = $this->getToken();
+        if (!$accessToken) {
+            throw new \RuntimeException('No access token given', 1568871911);
+        }
+        $client->setAccessToken($accessToken);
+
+        if ($client->isAccessTokenExpired()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            $this->setToken($client->getAccessToken());
+        }
+        return $client;
+    }
+
+    public function getToken()
+    {
+        $token = $this->registry->get(self::REGISTRY_KEY, 'token');
+
+        if ($token) {
+            return json_decode($token, true);
+        }
+        return null;
+    }
+
+    public function setToken(array $token)
+    {
+        $this->registry->set(self::REGISTRY_KEY, 'token', json_encode($token));
+    }
+}
---
+++ Classes/Command/SetupCredentialsCommand.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types = 1);
+
+namespace Mabolek\GoogleDriveFal\Command;
+
+use Mabolek\GoogleDriveFal\Api\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class SetupCredentialsCommand extends Command
+{
+
+    /**
+     * Defines the allowed options for this command
+     *
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Setup access')
+            ->addArgument('credentialsFile', InputArgument::REQUIRED, 'Path to the credentials');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $credentialsFile = $input->getArgument('credentialsFile');
+        if (!is_file($credentialsFile)) {
+            $io->error(sprintf('File "%s" does not exist', $credentialsFile));
+        } else {
+            $typo3Client = GeneralUtility::makeInstance(Client::class);
+            $config = json_decode(file_get_contents($credentialsFile), true);
+            $typo3Client->setAuthConfig($config);
+
+            $googleClient = new \Google_Client();
+            $googleClient->setApplicationName('Google Drive for TYPO3');
+            $googleClient->setScopes(Client::SCOPES);
+            $googleClient->setAccessType('offline');
+            $googleClient->setAuthConfig($config);
+
+            $question = new Question(sprintf(
+                'Open the following link in your browser: %s and enter the verification link:',
+                $googleClient->createAuthUrl()
+            ));
+            $authCode = $io->askQuestion($question);
+
+            $accessToken = $googleClient->fetchAccessTokenWithAuthCode($authCode);
+
+            $typo3Client->setToken($accessToken);
+
+            $googleClient->setAccessToken($accessToken);
+
+            $io->success('Successfully configured!');
+        }
+    }
+}
