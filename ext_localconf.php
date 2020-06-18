2fbc28ff23fdcde495047d5eca6586f699be9bc6
mabolek <mathias@lilio.com>
2020-06-18 19:38:49 +0200

    [FEATURE] Automatically create a useable processing folder
    When the file storage record is saved, a DataHandlerHook will check that the directory is not on a Google Drive™, and that it is public and writeable.
    
    If the processing folder does not pass this test or is an empty string it will be set to "0:typo3temp/assets/_processed_storage_[UID]" where [UID] is the UID of the file storage record.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,8 @@
 <?php
 
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['google_drive_fal'] =
+    \Mabolek\GoogleDriveFal\Hooks\FileStorageDataHandlerHook::class;
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers'][\Mabolek\GoogleDriveFal\Driver\GoogleDriveDriver::DRIVER_TYPE] = [
     'class' => \Mabolek\GoogleDriveFal\Driver\GoogleDriveDriver::class,
     'flexFormDS' => 'FILE:EXT:google_drive_fal/Configuration/FlexForm/GoogleDriveStorageConfigurationFlexForm.xml',
